f972992b08d359620931fb84004b1b81dbe0172b
alwayss4u <38743299+alwayss4u@users.noreply.github.com>
2018-06-04 19:54:17 +0900

    20180604

--- app/src/main/java/com/roy/taxicharge/ResultActivity.java
+++ app/src/main/java/com/roy/taxicharge/ResultActivity.java
@@ -1,21 +1,28 @@
 package com.roy.taxicharge;
 
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.nhn.android.maps.NMapActivity;
 import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapOverlay;
 import com.nhn.android.maps.NMapOverlayItem;
 import com.nhn.android.maps.NMapView;
 import com.nhn.android.maps.maplib.NGeoPoint;
 import com.nhn.android.maps.nmapmodel.NMapError;
+import com.nhn.android.maps.overlay.NMapPOIdata;
 import com.nhn.android.maps.overlay.NMapPOIitem;
 import com.nhn.android.maps.overlay.NMapPathData;
+import com.nhn.android.maps.overlay.NMapPathLineStyle;
+import com.nhn.android.mapviewer.overlay.NMapCalloutOverlay;
 import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
 import com.nhn.android.mapviewer.overlay.NMapPathDataOverlay;
 import com.nhn.android.mapviewer.overlay.NMapResourceProvider;
 import java.io.BufferedReader;
@@ -36,23 +43,36 @@ public class ResultActivity extends NMapActivity {
     double[] longitudes;
     double[] latitudes;
 
-    NMapOverlayManager nMapOverlayManager;
-    //nMapViewerResourceProvider nMapViewerResourceProvider = new nMapViewerResourceProvider(this);
+    NMapOverlayManager mOverlayManager;
+    NMapViewerResourceProvider mMapViewerResourceProvider;
     NMapController mMapController;
+
+    int markerId = NMapPOIflagType.PIN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        //NMapOverlayManager mOverlayManager = new NMapOverlayManager(this, mMapView, nMapViewerResourceProvider);
 
         longitudes = getIntent().getDoubleArrayExtra("longitude");
         latitudes = getIntent().getDoubleArrayExtra("latitude");
 
         mMapView = new NMapView(this);
-        setContentView(mMapView);
         mMapView.setClientId(CLIENT_ID);
+        setContentView(mMapView);
         mMapView.setClickable(true);
+
+        // create resource provider
+        mMapViewerResourceProvider = new NMapViewerResourceProvider(this);
+
+// create overlay manager
+        mOverlayManager = new NMapOverlayManager(this, mMapView, mMapViewerResourceProvider);
+
+        // register listener for map state changes
+        //mMapView.setOnMapStateChangeListener(onMapViewStateChangeListener);
+        //mMapView.setOnMapViewTouchEventListener(onMapViewTouchEventListener);
+
+
         mMapView.setEnabled(true);
         mMapView.setFocusable(true);
         mMapView.setFocusableInTouchMode(true);
@@ -61,50 +81,36 @@ public class ResultActivity extends NMapActivity {
 
         // use map controller to zoom in/out, pan and set map center, zoom level etc.
         mMapController = mMapView.getMapController();
-
+        // use built in zoom controls
         mMapView.setBuiltInZoomControls(true, null);
         //지도를 확대하거나 축소할 때 내장된 컨트롤러를 사용
+
         NMapPathData pathData = new NMapPathData(longitudes.length);
 
         pathData.initPathData();
 
-        for(int i= 0; i<longitudes.length; i++)
-            pathData.addPathPoint(latitudes[i], longitudes[i], 0);
+        for(int i= 0; i<longitudes.length; i++){
+            if(i==0) {
+                pathData.addPathPoint(longitudes[i], latitudes[i], NMapPathLineStyle.TYPE_SOLID);
+                continue;
+            }
+            pathData.addPathPoint(longitudes[i], latitudes[i], 0);
+        }
+
+        String latitu = "";
+        String longitu = "";
+        for(int i=0; i< latitudes.length; i++){
+            latitu += latitudes[i];
+            longitu += longitudes[i];
+        }
+        Log.i("latitudes", latitu);
+        Log.i("longitutds", longitu);
 
         pathData.endPathData();
 
+        NMapPathDataOverlay pathDataOverlay = mOverlayManager.createPathDataOverlay(pathData);
 
-
-//        NMapPathDataOverlay pathDataOverlay = nMapOverlayManager.createPathDataOverlay();
-
-//        String clientId = "YOUR_CLIENT_ID";//애플리케이션 클라이언트 아이디값";
-//        String clientSecret = "YOUR_CLIENT_SECRET";//애플리케이션 클라이언트 시크릿값";
-//        try {
-//            String addr = URLEncoder.encode("불정로 6", "UTF-8");
-//            String apiURL = "https://openapi.naver.com/v1/map/geocode?query=" + addr; //json
-//            //String apiURL = "https://openapi.naver.com/v1/map/geocode.xml?query=" + addr; // xml
-//            URL url = new URL(apiURL);
-//            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-//            con.setRequestMethod("GET");
-//            con.setRequestProperty("X-Naver-Client-Id", clientId);
-//            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-//            int responseCode = con.getResponseCode();
-//            BufferedReader br;
-//            if(responseCode==200) { // 정상 호출
-//                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-//            } else {  // 에러 발생
-//                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-//            }
-//            String inputLine;
-//            StringBuffer response = new StringBuffer();
-//            while ((inputLine = br.readLine()) != null) {
-//                response.append(inputLine);
-//            }
-//            br.close();
-//            System.out.println(response.toString());
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
+        pathDataOverlay.showAllPathData(0);
     }
 
     public void onMapInitHandler(NMapView mapView, NMapError errorInfo) {
@@ -113,6 +119,24 @@ public class ResultActivity extends NMapActivity {
         } else { // fail
             Log.e(LOG_TAG, "onMapInitHandler: error=" + errorInfo.toString());
         }
+    }
+
+    public void onCalloutClick(NMapPOIdataOverlay poiDataOverlay, NMapPOIitem item) {
+        // [[TEMP]] handle a click event of the callout
+        Toast.makeText(this, "onCalloutClick: " + item.getTitle(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onFocusChanged(NMapPOIdataOverlay poiDataOverlay, NMapPOIitem item) {
+        if (item != null) {
+            Log.i(LOG_TAG, "onFocusChanged: " + item.toString());
+        } else {
+            Log.i(LOG_TAG, "onFocusChanged: ");
+        }
+    }
+
+    public NMapCalloutOverlay onCreateCalloutOverlay(NMapOverlay itemOverlay, NMapOverlayItem overlayItem, Rect itemBounds) {
+        // set your callout overlay
+        return new NMapCalloutBasicOverlay(itemOverlay, overlayItem, itemBounds);
     }
 
 }
